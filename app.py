a1dc74d297293098797a6f9189ddf9464e267068
irab21 <65721379+irab21@users.noreply.github.com>
2021-06-17 11:40:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -709,8 +709,8 @@ if password== 'datacore@2020':
 	total_selections1=data['Client'].value_counts()
 	total_selections1=data.groupby('Client').count()
 	total_selections1=total_selections1.sum()
-	st.write(total_selections1)
-	st.write(total_selections)
+	#st.write(total_selections1)
+	#st.write(total_selections)
 
 	clients= data['Client'].value_counts()
 	#titles
@@ -733,7 +733,7 @@ if password== 'datacore@2020':
 
 	st.markdown('### Number Of Positive Conversions, Negative Conversions And Pending Conversions')
 	st.write('\n\n')
-	st.write('\n\n As can be seen, out of %s total selections:\n\n %s candidates were offered \n\n Out of which, %s Candidates were Positively Converted \n\n %s Candidates were not Converted \n\n %s Candidate Conversions are still Pending '%(total_selections1.values[0],offer_count.values[1],total_selections.values[2,2],total_selections.values[0,0],total_selections.values[1,1]))
+	st.write('\n\n As can be seen, out of %s total selections:\n\n %s candidates were offered \n\n Out of which, %s Candidates were Positively Converted \n\n %s Candidates were not Converted \n\n %s Candidate Conversions are still Pending '%(total_selections1.values[0],offer_count.values[1],total_selections.values[1,1],total_selections.values[0,0],0))
 	if st.sidebar.checkbox('Visual',True, key=4):
 		fig1=px.pie(status_count, values='Count',names='Status')
 		st.plotly_chart(fig1)
