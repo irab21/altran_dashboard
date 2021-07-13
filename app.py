84279e43997ff7a577e1545c51743b5d256f5103
irab21 <65721379+irab21@users.noreply.github.com>
2021-07-13 18:07:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ if password == "altran@2020":
 
 	st.markdown('### Number Of Positive Conversions, Negative Conversions And Pending Conversions')
 	st.write('\n\n')
-	st.write('\n\n As can be seen, out of %s total selections: \n\n Out of which, %s Candidates were Positively Converted \n\n %s Candidates were not Converted '%(total_selections1.values[0],total_selections.values[0,0],total_selections.values[0,1]))
+	st.write('\n\n As can be seen, out of %s total selections: \n\n Out of which, %s Candidates were Positively Converted \n\n %s Candidates were not Converted '%(total_selections1.values[0],total_selections.values[0,1],total_selections.values[0,0]))
 	if st.sidebar.checkbox('Visual',True, key=4):
 		fig1=px.pie(status_count, values='Count',names='Status')
 		st.plotly_chart(fig1)
