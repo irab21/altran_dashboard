3592cece0a0d4d0f8e0392e487605317c99a1f74
irab21 <65721379+irab21@users.noreply.github.com>
2021-08-05 17:10:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1343,13 +1343,13 @@ if password== 'deloitte@2020':
 	offer_count=data['Offer Date'].value_counts()
 	offer_count=data.groupby('Offer Date').count()
 	offer_count=offer_count.sum()
-	st.write(offer_count)
-	st.write(total_selections1)
+	#st.write(offer_count)
+	#st.write(total_selections1)
 
 
 	st.markdown('### Number Of Positive Conversions, Negative Conversions And Pending Joinings')
 	st.write('\n\n')
-	st.write('\n\n As can be seen, out of %s total selections:\n\n %s candidates were offered \n\n Out of which, %s Candidates were Positively Converted \n\n %s Candidates were not Converted \n\n %s Candidates are yet to Join '%(total_selections1.values[0],offer_count.values[1],total_selections.values[2,2],total_selections.values[0,0],total_selections.values[1,1]))
+	st.write('\n\n As can be seen, out of %s total selections:\n\n %s candidates were offered \n\n Out of which, %s Candidates were Positively Converted \n\n %s Candidates were not Converted \n\n %s Candidates are yet to Join '%(total_selections1.values[0],offer_count.values[1],total_selections.values[1,1],0,0))
 	if st.sidebar.checkbox('Visual',True, key=4):
 		fig1=px.pie(status_count, values='Count',names='Status')
 		st.plotly_chart(fig1)
