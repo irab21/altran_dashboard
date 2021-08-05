4965d753b5d3ea0bfba32906338a0b394b3b589a
irab21 <65721379+irab21@users.noreply.github.com>
2021-08-05 13:30:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1118,7 +1118,7 @@ if password== 'delhivery@2020':
 	st.markdown('### By Posterity Better Solutions')
 	st.sidebar.markdown('### A Review of the past year ')
 
-	st.sidebar.markdown("### Number Of Positive Coneversions, Negative Conversions, and Pending Conversions")
+	st.sidebar.markdown("### Number Of Positive Coneversions, Negative Conversions, and Pending Joinings")
 	
 	#newdataframe
 	status_count= data['Status'].value_counts()
@@ -1129,9 +1129,9 @@ if password== 'delhivery@2020':
 	#st.write(offer_count)
 
 
-	st.markdown('### Number Of Positive Conversions, Negative Conversions And Pending Conversions')
+	st.markdown('### Number Of Positive Conversions, Negative Conversions And Joining Pending')
 	st.write('\n\n')
-	st.write('\n\n As can be seen, out of %s total selections:\n\n %s candidates were offered \n\n Out of which, %s Candidates were Positively Converted \n\n %s Candidates were not Converted \n\n %s Candidate Conversions are still Pending '%(total_selections1.values[0],offer_count.values[1],total_selections.values[2,2],total_selections.values[0,0],total_selections.values[1,1]))
+	st.write('\n\n As can be seen, out of %s total selections:\n\n %s candidates were offered \n\n Out of which, %s Candidates were Positively Converted \n\n %s Candidates were not Converted \n\n %s Candidate Conversions are yet to join. '%(total_selections1.values[0],offer_count.values[1],total_selections.values[2,2],total_selections.values[0,0],total_selections.values[1,1]))
 	if st.sidebar.checkbox('Visual',True, key=4):
 		fig1=px.pie(status_count, values='Count',names='Status')
 		st.plotly_chart(fig1)
@@ -1269,7 +1269,7 @@ if password== 'delhivery@2020':
 	offer_percent=round(offers/selections*100)
 	joining_percent=round(joinings/offers*100)
 	#st.write(pending)
-	st.write("As can be seen, out of total %s Selections, %s Candidates Joined"%(total_selections1.values[0],total_selections.values[1,1]))
+	st.write("As can be seen, out of total %s Selections, %s Candidates Joined"%(total_selections1.values[0],total_selections.values[2,2]))
 	st.write("There was a "+str(offer_percent)+"% Selection Conversion and "+str(joining_percent)+"% Offer Conversion")
 	df=pd.DataFrame({'Stage':['Selections','Offers','Joining'],'Number':[[selections],[offers],[joinings]]})
 #	st.write(df)
