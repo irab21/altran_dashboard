0df2dc32e2dda94accc4a2af49a9392c581b0a89
irab21 <65721379+irab21@users.noreply.github.com>
2021-08-12 14:36:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1729,14 +1729,14 @@ if password== 'grantthornton@2020':
 
 
 
-if password== 'r1rcm@2020':
+if password== 'guardian@2021':
 
 	image_url='posterityfinal.png'
 	image= Image.open(image_url) 
 	st.image(image,width=350)
 
 
-	DATA_URL= 'file10.xlsx'
+	DATA_URL= 'guardian.xlsx'
 
 	#@st.cache(persist =True)
 	def load_data():
@@ -1758,13 +1758,13 @@ if password== 'r1rcm@2020':
 
 	clients= data['Client'].value_counts()
 	#titles
-	st.title('Client Report April-June 2021')
+	st.title('Client Report April-July 2021')
 	st.sidebar.title('%s  '% (clients.index[0]))
 
 	st.markdown('### By Posterity Better Solutions')
-	st.sidebar.markdown('### A Review of the past year ')
+	st.sidebar.markdown('### A Review of the past 4 months ')
 
-	st.sidebar.markdown("### Number Of Positive Coneversions, Negative Conversions, and Pending Conversions")
+	st.sidebar.markdown("### Number Of Positive Coneversions, Negative Conversions, and Pending Joining")
 	
 		#newdataframe
 	status_count= data['Status'].value_counts()
@@ -1775,10 +1775,10 @@ if password== 'r1rcm@2020':
 	#st.write(offer_count)
 
 
-	st.markdown('### Number Of Positive Conversions, Negative Conversions And Pending Conversions')
+	st.markdown('### Number Of Positive Conversions, Negative Conversions And Pending Joining')
 	st.write('\n\n')
 	st.write('\n\n As can be seen, out of %s total selections:\n\n %s candidates were offered \n\n Out of which, %s Candidates were Positively Converted \n\n %s Candidates were not Converted \n\n %s Candidate Conversions are still Pending '%(total_selections1.values[0],offer_count.values[1],total_selections.values[2,2],total_selections.values[0,0],total_selections.values[1,1]))
-	if st.sidebar.checkbox('Visual',True, key=4):
+	 if st.sidebar.checkbox('Visual',True, key=4):
 		fig1=px.pie(status_count, values='Count',names='Status')
 		st.plotly_chart(fig1)
 
